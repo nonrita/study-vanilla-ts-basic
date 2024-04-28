0d047303942b01a6a813eb8780baa8a88294d86c
nonrita <2220025@ecc.ac.jp>
2024-04-28 19:44:19 +0900

    オブジェクトに方をつける方法

---
+++ src/main.js
@@ -0,0 +1,17 @@
+var hasValue = true;
+var count = 10;
+var float = 3.14;
+var negative = -0.12;
+var single = 'hello';
+var double = "hello";
+var back = "hello";
+var hello;
+hello = 'hello';
+var person = {
+    name: {
+        first: "Jack",
+        last: "Smith"
+    },
+    age: 21,
+};
+console.log(person.name);
