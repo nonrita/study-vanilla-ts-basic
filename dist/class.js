84e2955ad01e32a47961ee8e21fde3cd049d7ae5
nonrita <2220025@ecc.ac.jp>
2024-04-29 03:56:39 +0900

    public修飾子とprivate修飾子を使用して、アクセスを制限する方法

--- dist/class.js
+++ dist/class.js
@@ -1,20 +1,32 @@
 "use strict";
 class Person {
-    constructor(initName) {
+    constructor(initName, initAge) {
         Object.defineProperty(this, "name", {
             enumerable: true,
             configurable: true,
             writable: true,
             value: void 0
         });
+        Object.defineProperty(this, "age", {
+            enumerable: true,
+            configurable: true,
+            writable: true,
+            value: void 0
+        });
         this.name = initName;
+        this.age = initAge;
+    }
+    incrementAge() {
+        this.age += 1;
     }
     greeting() {
-        console.log(`Hello My name is ${this.name}`);
+        console.log(`Hello My name is ${this.name}. I am ${this.age} years old`);
     }
 }
 ;
-const quill = new Person('Quill');
+const quill = new Person('Quill', 20);
+quill.incrementAge();
+// quill.age = 41;
 quill.greeting();
 const anotherQuill = {
     anotherGreeting: quill.greeting
--- src/class.ts
+++ src/class.ts
@@ -1,16 +1,24 @@
 class Person {
     name: string;
+    private age: number;
 
-    constructor(initName: string) {
-        this.name = initName
+    constructor(initName: string, initAge: number) {
+        this.name = initName;
+        this.age = initAge;
+    }
+
+    incrementAge() {
+        this.age += 1;
     }
 
     greeting (this: Person ) {
-        console.log(`Hello My name is ${this.name}`);
+        console.log(`Hello My name is ${this.name}. I am ${this.age} years old`);
     }
 };
 
-const quill = new Person('Quill');
+const quill = new Person('Quill', 20);
+quill.incrementAge();
+// quill.age = 41;
 quill.greeting();
 
 const anotherQuill = {
