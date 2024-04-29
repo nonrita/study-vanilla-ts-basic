fb54723cbabed61ea73e5329b65e70a0b244e16b
nonrita <2220025@ecc.ac.jp>
2024-04-29 10:17:02 +0900

    interfaceで関数の型を表現する方法 -コールシグネチャ, コンストラクタシグネチャ

--- dist/interface.js
+++ dist/interface.js
@@ -1,4 +1,8 @@
 "use strict";
+let addFunc;
+addFunc = (n1, n2) => {
+    return n1 + n2;
+};
 class Developer {
     constructor(name, age, experience) {
         Object.defineProperty(this, "name", {
